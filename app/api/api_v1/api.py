36ffb475fb18762001cfa7e9869cefefb84f5767
Imran <imran.biswas@nimblework.com>
2024-12-29 23:13:09 +0530

    refactor: update forms endpoint import to singular form

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
-from .endpoints import auth, forms
+from .endpoints import auth, form
 
 
 api_router = APIRouter()
 
 api_router.include_router(auth.router, prefix="/auth", tags=["Authentication"])
-api_router.include_router(forms.router, prefix="/forms", tags=["Form Management"])
+api_router.include_router(form.router, prefix="/form", tags=["Form Management"])
