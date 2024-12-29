87e8188a63cef8d4db6fb06f4e70a6b35b6e6ddc
Imran <imran.biswas@nimblework.com>
2024-12-29 23:12:54 +0530

    feat: update database engine initialization to use settings for DATABASE_URI

--- app/core/database.py
+++ app/core/database.py
@@ -1,12 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine(DATABASE_URL)  # fetch db env from env
+from app.settings.config import settings
+
+engine = create_engine(settings.DATABASE_URI)  # fetch db env from env
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 class DB:
-  def get_db():
+  def get_db(self):
     db = SessionLocal()
     try:
         yield db
