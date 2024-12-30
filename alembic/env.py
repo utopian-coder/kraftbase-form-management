9cb924d8db605dc51e7a2ff62482356d332a2b3b
Imran <imran.biswas@nimblework.com>
2024-12-30 23:33:37 +0530

    feat: update Alembic configuration to use DATABASE_URI from settings

--- alembic/env.py
+++ alembic/env.py
@@ -9,6 +9,10 @@ from alembic import context
 # access to the values within the .ini file in use.
 config = context.config
 
+from app.settings.config import settings
+
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URI)
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
