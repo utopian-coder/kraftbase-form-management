1c84397d0823ed7390f82d2a880a25116d7c66e6
Imran <imran.biswas@nimblework.com>
2024-12-30 11:50:35 +0530

    feat: enhance session deletion logic to validate token existence before removal

--- app/services/user_session.py
+++ app/services/user_session.py
@@ -5,6 +5,7 @@ from uuid import UUID, uuid4
 from fastapi import Cookie, Depends, HTTPException
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
+from sqlalchemy import delete
 
 from app.core.database import db
 from app.models.session import UserSession
@@ -46,7 +47,10 @@ class UserSessionService:
     session_token = cookies.session_token
     _token = UUID(bytes=base64.urlsafe_b64decode(session_token))
 
-    print(f"_token: {_token}")
-
     # Delete the user's session_token in the database
-    db.query(UserSession).filter(UserSession.token == _token).delete()
+    _token_in_db = db.query(UserSession).filter(UserSession.token == _token).first()
+    if not _token_in_db:
+        raise HTTPException(status_code=401, detail="Invalid session or user not found")
+
+    _token_in_db.delete()
+    db.commit()
